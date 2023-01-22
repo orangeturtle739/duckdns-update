370235b22e6fee2a94b7d39f3355565ab8fedc63
Jacob Glueck <swimgiraffe435@gmail.com>
2023-01-21 19:41:11 -0800

    Use overlays

--- flake.nix
+++ flake.nix
@@ -5,24 +5,31 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ [ "armv7l-linux" ]) (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        commonBuildInputs = with pkgs; [ pkg-config openssl ];
-        duckdns-update = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "duckdns-update";
-          version = "0.1.0";
-          src = self;
-          cargoSha256 = "6zAay+LGSJ0RcQxMQ55sogQngkSyPtJI0SOpAykV6zo=";
-          builtInputs = commonBuildInputs;
+    let
+      withPkgs = pkgs:
+        let
+          commonBuildInputs = with pkgs; [ pkg-config openssl ];
+          duckdns-update = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "duckdns-update";
+            version = "0.1.0";
+            src = self;
+            cargoSha256 = "6zAay+LGSJ0RcQxMQ55sogQngkSyPtJI0SOpAykV6zo=";
+            builtInputs = commonBuildInputs;
+          };
+        in {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.rustfmt pkgs.cargo ] ++ commonBuildInputs;
+            shellHook = ''
+              export RUST_BACKTRACE=full
+            '';
+          };
+          packages.default = duckdns-update;
         };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.rustfmt pkgs.cargo ] ++ commonBuildInputs;
-          shellHook = ''
-            export RUST_BACKTRACE=full
-          '';
+    in (flake-utils.lib.eachSystem
+      (flake-utils.lib.defaultSystems ++ [ "armv7l-linux" ])
+      (system: withPkgs (import nixpkgs { inherit system; }))) // {
+        overlays.default = final: prev: {
+          duckdns-update = (withPkgs prev).packages.default;
         };
-        defaultPackage = duckdns-update;
-      });
+      };
 }
